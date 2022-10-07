ea98ed3fecb38d3f4e164b464b5b993b24b1db8c
wvsotskyi <wvsotskyi@gmail.com>
2022-10-07 22:47:39 +0300

    m

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import * as fs from `node:fs`;
 
 fs.readFile('/index.html', function(err, html) {
     if (err) {
-        throw err;
+        return http.createServer(function(request, response) {
+            response.writeHeader(200, { "Content-Type": "text/html" });
+            response.end(`hello world`);
+        }).listen(process.env.PORT);
     }
-    http.createServer(function(request, response) {
+    return http.createServer(function(request, response) {
         response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
+        response.end(html);
     }).listen(process.env.PORT);
 });

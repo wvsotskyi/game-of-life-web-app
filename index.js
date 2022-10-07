ac2bcb80255f317245db2a16cb08fef0024cfdc2
wvsotskyi <wvsotskyi@gmail.com>
2022-10-07 22:59:52 +0300

    again

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import * {http} from "http";
+import * as fs from 'node:fs';
+import * as http from 'node:http';
+import * as path from 'node:path';
 
 http.createServer(function (request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });

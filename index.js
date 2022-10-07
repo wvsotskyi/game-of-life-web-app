3bdfec69b587454ff5411fd58b17bd4259e2f027
wvsotskyi <wvsotskyi@gmail.com>
2022-10-07 23:41:51 +0300

    remove

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 import * as fs from 'node:fs';
 import * as http from 'node:http';
-import * as path from 'node:path';
 
-const MIME_TYPES = {
-    default: 'application/octet-stream',
-    html: 'text/html; charset=UTF-8',
-    js: 'application/javascript; charset=UTF-8',
-};
+fs.readFile('./index.html', function(err, html) {
 
-const STATIC_PATH = path.join(process.cwd(), './static');
+    if (err) throw err;
 
-const prepareFile = async(url) => {
-    const paths = [STATIC_PATH, url];
-    if (url.endsWith('/')) paths.push('index.html');
-    const filePath = path.join(...paths);
-    const pathTraversal = !filePath.startsWith(STATIC_PATH);
-    const exists = await fs.promises.access(filePath).then(...toBool);
-    const found = !pathTraversal && exists;
-    const streamPath = found ? filePath : STATIC_PATH + '/404.html';
-    const ext = path.extname(streamPath).substring(1).toLowerCase();
-    const stream = fs.createReadStream(streamPath);
-    return { found, ext, stream };
-};
-
-http.createServer(async(request, response) => {
-    const file = await prepareFile(req.url);
-    const statusCode = file.found ? 200 : 404;
-    const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
-    response.writeHead(200, { 'Content-Type': mimeType });
-    file.stream.pipe(response);
-    response.end(`${request.method} ${request.url} ${statusCode}`);
-}).listen(process.env.PORT);
+    http.createServer(function(request, response) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(process.env.PORT);
+});

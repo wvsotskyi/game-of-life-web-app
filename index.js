8d5653cbf6c3f90f5c0206b053acc843160090f2
wvsotskyi <wvsotskyi@gmail.com>
2022-10-07 23:01:31 +0300

    finallly

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ import * as fs from 'node:fs';
 import * as http from 'node:http';
 import * as path from 'node:path';
 
-http.createServer(function (request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(`html`);
-}).listen(process.env.PORT);
+
+
+fs.readFile('./index.html', function (err, html) {
+
+    if (err) throw err;   
+     
+    http.createServer(function (request, response) {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.end(html);
+    }).listen(process.env.PORT);
+});

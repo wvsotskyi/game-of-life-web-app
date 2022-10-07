5a7251005cb14699ab796d60d085d798a51aa3fe
wvsotskyi <wvsotskyi@gmail.com>
2022-10-07 22:52:02 +0300

    no

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-import * as http from 'node:http';
-import * as fs from `node:fs`;
+const http = require('http');
+const cloud = require('cloud');
 
-
-fs.readFile('/index.html', function(err, html) {
-    if (err) {
-        return http.createServer(function(request, response) {
-            response.writeHeader(200, { "Content-Type": "text/html" });
-            response.end(`hello world`);
-        }).listen(process.env.PORT);
-    }
-    return http.createServer(function(request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.end(html);
-    }).listen(process.env.PORT);
-});
+http.createServer(function(request, response) {
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    response.end(`html`);
+}).listen(process.env.PORT);

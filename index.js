acccce14f382590ec737f62a44fe80c36cc44692
wvsotskyi <wvsotskyi@gmail.com>
2022-10-08 09:21:43 +0300

    express

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ import * as fs from 'node:fs';
 import * as http from 'node:http';
 import express from "express";
 import * as path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+const __dirname = dirname(fileURLToPath(
+    import.meta.url));
+const PORT = process.env.PORT || 8080;
 var app = express();
-app.use(express.static(__dirname + 'client'));
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
-}).listen(process.env.PORT);
 
+console.log(__dirname);
 
-app.get('/client/code.js', function(req, res) {
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname + '/style.css'));
+});
+
+app.get('/code.js', function(req, res) {
     res.sendFile(path.join(__dirname + '/code.js'));
-}).listen(process.env.PORT);
+});
+
+app.get('/', function(req, res) {
+    res.redirect('index.html');
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})

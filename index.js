882ab64df7ee2e09cc9235d4824905b9e9b4f56e
wvsotskyi <wvsotskyi@gmail.com>
2022-10-08 08:35:33 +0300

    t

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ import * as fs from 'node:fs';
 import * as http from 'node:http';
 import connect from "connect";
 
+
 http.createServer(function(request, response) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    html = fs.readFileSync('./index.html');
-    response.end(html);
+    let html = fs.openSync('./index.html');
+    let page = fs.readFileSync('./index.html');
+    response.end(page);
 }).listen(process.env.PORT);

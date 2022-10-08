7538ffb764fca7cfb968f6ff1287917d25341619
wvsotskyi <wvsotskyi@gmail.com>
2022-10-08 08:28:23 +0300

    read async

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import * as fs from 'node:fs';
 import * as http from 'node:http';
+import connect from "connect";
 
-fs.readFile('./index.html', function(err, html) {
-
-    if (err) throw err;
-
-    http.createServer(function(request, response) {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(process.env.PORT);
-});
+http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    html = fs.readFileSync('./index.html');
+    response.end(html);
+}).listen(process.env.PORT);

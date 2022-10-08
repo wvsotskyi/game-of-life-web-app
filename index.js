64464ce4da72e5d818113319e95b12f8d23923a7
wvsotskyi <wvsotskyi@gmail.com>
2022-10-08 09:00:53 +0300

    express

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import * as fs from 'node:fs';
 import * as http from 'node:http';
-import connect from "connect";
+import express from "express";
+import * as path from 'node:path';
+
+var app = express();
+app.use(express.static(__dirname + 'client'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client/index.html'));
+}).listen(process.env.PORT);
 
 
-http.createServer(function(request, response) {
-    response.writeHead(200, { "Content-Type": "text/html" });
-    let html = fs.openSync('./index.html');
-    let page = fs.readFileSync('./index.html');
-    response.end(page);
+app.get('/client/code.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/code.js'));
 }).listen(process.env.PORT);
